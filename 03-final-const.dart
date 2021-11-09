1a01c2449d0cd9286ba190718e38c04da006bdf7
Neesh <avaneeshsamsi@gmail.com>
2021-11-09 19:51:52 +0530

    Video 3 complete

--- 03-final-const.dart
+++ 03-final-const.dart
@@ -1,23 +1,23 @@
 void main() {
-  /*
+/*
   var name;
   print(name); // All variables are null by default since everything is a Dart Object and if there's nothing in the object it's null.
-  */
+*/
 
-  /*
+/*
   final : variable
   Final is a variable but it can be reassigned only once.
   Final is used in Classes. It can be initialized without a function if it is required in a contructor so it can be assigned once through the calling of the Class Contructor. From then on it acts as a const. (example below.)
-  */
+*/
 
-  /*  
+/*  
   const : variable // Variable & Const ? varibale is just a a way to store data in memory.
   Const is a variable but it cannot be reassigned.
-  */
+*/
 
-  /*
+/*
   Both final and const are immutable. Once a data has been put into memory, the same location will not be used to store any other data in the future.
-  */
+*/
 
 
   // final String name = "Neesh";
