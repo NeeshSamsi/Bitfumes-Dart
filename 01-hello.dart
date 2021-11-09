6b25a1d733ddaad1446f56c8c239ce8750bf15af
Neesh <avaneeshsamsi@gmail.com>
2021-11-09 19:24:35 +0530

    Personal notes from first video

--- 01-hello.dart
+++ 01-hello.dart
@@ -1,3 +1,15 @@
+/*
+Install
+https://dart.dev/get-dart
+Windows install uses Chocolatey (https://chocolatey.org/install), Linux (apt-get) and Mac uses Brew (https://brew.sh).
+Use Get Dart docs to add the Dart SDK to your path.
+VS Code extensions - Flutter extension pack or Dart extension pack to get everything running.
+*/
+
+/*
+Dart is  a mixture of all the good things from various languages. The print() is like py, function declaration is like js and concept of main entry point and classes is like java or c.
+*/
+
 void main() {
   print("Hello World");
 }
