cce8ca24eda62438abeda46a6d10a0c9848e53b7
Neesh <avaneeshsamsi@gmail.com>
2021-11-11 15:41:23 +0530

    Video 7 complete

---
+++ 07-string-methods-properties.dart
@@ -0,0 +1,17 @@
+main() {
+  String name = "Neesh Samsi";
+
+  // print(name.toUpperCase());
+  // print(name.toLowerCase());
+
+  // print(name.length);
+  // print(name.trim());
+  // print(name.trim().length);
+
+  // print(name.replaceAll('Neesh', 'Shrav'));
+
+  // print(name.codeUnits); // Returns array of ASCII code for each letter
+
+  // print(name.isEmpty);
+  // print(name.isNotEmpty);
+}
